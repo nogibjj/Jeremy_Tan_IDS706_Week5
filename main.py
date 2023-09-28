64d728dacdb6b85f1a59c9328bb2b97836a45db8
jeremymtan <jemtan@ucsc.edu>
2023-09-28 01:54:32 -0400

    modified cli and etc

--- main.py
+++ main.py
@@ -1,19 +1,104 @@
-"""
-ETL-Query script
-"""
-
+"""handles cli commands"""
+import sys
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import (
+    update_record,
+    delete_record,
+    create_record,
+    general_query,
+    read_data,
+)
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def handle_arguments(args):
+    """add action based on inital calls"""
+    parser = argparse.ArgumentParser(description="ETL-Query script")
+    parser.add_argument(
+        "action",
+        choices=[
+            "extract",
+            "transform_load",
+            "update_record",
+            "delete_record",
+            "create_record",
+            "general_query",
+            "read_data",
+        ],
+    )
+    args = parser.parse_args(args[:1])
+    print(args.action)
+    if args.action == "update_record":
+        parser.add_argument("record_id", type=int)
+        parser.add_argument("server")
+        parser.add_argument("seconds_before_next_point", type=int)
+        parser.add_argument("day")
+        parser.add_argument("opponent")
+        parser.add_argument("game_score")
+        parser.add_argument("sets", type=int)
+        parser.add_argument("game")
 
-# Query
-print("Querying data...")
-query()
+    if args.action == "create_record":
+        parser.add_argument("server")
+        parser.add_argument("seconds_before_next_point", type=int)
+        parser.add_argument("day")
+        parser.add_argument("opponent")
+        parser.add_argument("game_score")
+        parser.add_argument("sets", type=int)
+        parser.add_argument("game")
+
+    if args.action == "general_query":
+        parser.add_argument("query")
+
+    if args.action == "delete_record":
+        parser.add_argument("record_id", type=int)
+
+    # parse again with ever
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    """handles all the cli commands"""
+    args = handle_arguments(sys.argv[1:])
+
+    if args.action == "extract":
+        print("Extracting data...")
+        extract()
+    elif args.action == "transform_load":
+        print("Transforming data...")
+        load()
+    elif args.action == "update_record":
+        update_record(
+            args.record_id,
+            args.server,
+            args.seconds_before_next_point,
+            args.day,
+            args.opponent,
+            args.game_score,
+            args.sets,
+            args.game,
+        )
+    elif args.action == "delete_record":
+        delete_record(args.record_id)
+    elif args.action == "create_record":
+        create_record(
+            args.server,
+            args.seconds_before_next_point,
+            args.day,
+            args.opponent,
+            args.game_score,
+            args.sets,
+            args.game,
+        )
+    elif args.action == "general_query":
+        general_query(args.query)
+    elif args.action == "read_data":
+        data = read_data()
+        print(data)
+    else:
+        print(f"Unknown action: {args.action}")
+
+
+if __name__ == "__main__":
+    main()
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -7,20 +7,46 @@ import sqlite3
 import csv
 import os
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab/data/GroceryDB_IgFPro.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
+# load the csv file and insert into a new sqlite3 database
+def load(dataset="data/serve_times.csv"):
+    """Transforms and Loads data into the local SQLite3 database"""
+
+    # prints the full working directory and path
     print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    conn = sqlite3.connect('GroceryDB.db')
+    payload = csv.reader(open(dataset, newline=""), delimiter=",")
+    # skips the header of csv
+    next(payload)
+    conn = sqlite3.connect("ServeTimesDB.db")
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS ServeTimesDB")
+    c.execute(
+        """
+        CREATE TABLE ServeTimesDB (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            server TEXT,
+            seconds_before_next_point INTEGER,
+            day TEXT,
+            opponent TEXT,
+            game_score TEXT,
+            sets INTEGER,
+            game TEXT
+        )
+    """
+    )
+    # insert
+    c.executemany(
+        """
+        INSERT INTO ServeTimesDB (
+            server, 
+            seconds_before_next_point, 
+            day, opponent, game_score, 
+            sets, 
+            game
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)""",
+        payload,
+    )
     conn.commit()
     conn.close()
-    return "GroceryDB.db"
-
+    return "ServeTimesDB.db"
